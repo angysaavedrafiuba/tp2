a338ae9b777387c95505da2d7d75e1614eec3626
angysaavedrafiuba <asaavedra@fi.uba.ar>
2020-07-18 19:44:50 -0300

    Se agregan algunos constructores

--- gestion_turnos.c
+++ gestion_turnos.c
@@ -26,6 +26,29 @@ struct gestion_turnos{
 	hash_t* turnos;
 };
 
+paciente_t* paciente_crear(char** strv){ //constructor pasado en la funcion csv_crear_estructura
+	paciente_t* paciente = malloc(sizeof(paciente_t));
+
+	if (!paciente) return NULL;
+
+	paciente->nombre = strv[0];
+	paciente->ingreso = strv[1];
+
+	return paciente;
+}
+
+doctor_t* doctor_crear(char** strv){ //constructor pasado en la funcion csv_crear_estructura
+	doctor_t* doctor = malloc(sizeof(doctor_t));
+
+	if (!doctor) return NULL;
+
+	doctor->nombre = strv[0];
+	doctor->especialidad = strv[1];
+	doctor->pacientes_atendidos = 0;
+
+	return doctor;
+}
+
 gestion_turnos_t* gestion_turnos_crear(const char* ruta_csv_doctores,const char* ruta_csv_pacientes){
 	//gestion_turnos_t* gestion_turnos = malloc...
 	//pacientes = csv_crear_arbol_pacientes...
--- gestion_turnos.h
+++ gestion_turnos.h
@@ -18,4 +18,10 @@ typedef struct gestion_turnos gestion_turnos_t;
 //crea el TDA gestion_turnos
 gestion_turnos_t* gestion_turnos_crear(const char* ruta_csv_doctores,const char* ruta_csv_pacientes);
 
+void pedir_turno(char* nombre_paciente, char* especialidad, char* urgencia); //quizá hay que cambiarle el valor de retorno dependiendo de como se impriman los mensajes
+
+void atender_siguiente(char* nombre_doctor);
+
+void imprimir_informe_doctores(char* inicio, char* fin);
+
 #endif
