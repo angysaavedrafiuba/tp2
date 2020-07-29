2b5db341feb74e8b51a51de7c50ec2bc19ad2bef
mark-1594 <maservin@fi.uba.ar>
2020-07-28 22:56:49 -0300

    se documenta csv.h

--- csv.h
+++ csv.h
@@ -5,6 +5,9 @@
 #include "lista.h"
 #include "strutil.h"
 
+/*  Se encarga de guardar los datos leidos de los archivos csv con 
+    la funcion que se pasa por parámetro
+*/
 bool csv_cargar_datos(const char *ruta_csv, bool (*cargar)(char **, void *),
                       void *extra);
 
